ca11c9f8a4cbc2df21b5f524c75c49411ea539a9
Jarod L Woods <jarodlwoods@gmail.com>
2021-02-17 12:12:18 -0600

    text

--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -6,39 +6,40 @@
     public class TacoParser
     {
         readonly ILog logger = new TacoLogger();
-        
         public ITrackable Parse(string line)
         {
             logger.LogInfo("Begin parsing");
 
-            // Take your line and use line.Split(',') to split it up into an array of strings, separated by the char ','
             var cells = line.Split(',');
 
-            // If your array.Length is less than 3, something went wrong
             if (cells.Length < 3)
             {
-                // Log that and return null
-                // Do not fail if one record parsing fails, return null
-                return null; // TODO Implement
+                return null; 
             }
-
-            // grab the latitude from your array at index 0
+            double longitude;
+            double latitude;
+            string name;
+            double.TryParse(cells[1], out longitude);
+            double.TryParse(cells[0], out latitude);
+            name = cells[2];
+            var taco = new TacoBell();
+            taco.Name = name;
+            var point = new Point();
+            point.Longitude = longitude;
+            point.Latitude = latitude;
+            taco.Location = point;
+            
+            return taco;
             // grab the longitude from your array at index 1
             // grab the name from your array at index 2
-
             // Your going to need to parse your string as a `double`
             // which is similar to parsing a string as an `int`
-
             // You'll need to create a TacoBell class
             // that conforms to ITrackable
-
             // Then, you'll need an instance of the TacoBell class
             // With the name and point set correctly
-
             // Then, return the instance of your TacoBell class
             // Since it conforms to ITrackable
-
-            return null;
         }
     }
 }
